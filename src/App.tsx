157b0ef4ed5335d0b6d3b3fab11f7a8e309902d3
PTPhongKMF <169053895+PTPhongKMF@users.noreply.github.com>
2025-10-01 00:04:14 +0700

    feat: add slide and exam page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Register from "./pages/Register";
 import Test from "src/pages/Test";
 import Profile from "src/pages/Profile";
 import Admin from "src/pages/Admin";
+import Slides from "src/pages/Slides/Slides";
+import Exams from "src/pages/Exams/Exams";
 
 function App() {
   return (
@@ -14,8 +16,12 @@ function App() {
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+
+        <Route path="/slides" element={<Slides />} />
+        <Route path="/exams" element={<Exams />} />
+
         <Route path="profile" element={<Profile />} />
-        <Route path="admin" element={<Admin />} /> 
+        <Route path="admin" element={<Admin />} />
 
         <Route path="test" element={<Test />} />
       </Route>
